a24d5cfa8109c4d78fb9a62db795395337ade3fd
horechek <horechek@gmail.com>
2019-02-17 21:43:24 +0300

    first ci

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import bar from './bar';
+import _ from 'lodash'
+
+bar();
+
+function component() {
+    let element = document.createElement('div');
+
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+    return element;
+}
+
+document.body.appendChild(component());
